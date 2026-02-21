c441488df438a1fe3c901a3b1cef7cbd0eceb01b
Taruna B V <152138059+TarunaBV@users.noreply.github.com>
2026-02-21 16:12:47 +0530

    Remove unused matplotlib and seaborn imports
    Removed unused imports for matplotlib and seaborn.

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sb
 
 from sklearn.datasets import load_iris
 st.title("Iris flower classification project")
